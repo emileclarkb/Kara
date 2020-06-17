c94643c1bf0a9c1401e62cf931de3ceecdb32caf
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-18 07:40:40 +1000

    added custom setup file

---
+++ src/Core/setup.py
@@ -0,0 +1,26 @@
+import subprocess
+
+# download all modules given in a requirements file
+def setup(path):
+    requirements = []
+
+    # read requirements
+    with open(path, 'r') as file:
+        # iterate lines
+        for line in file.readlines():
+            pkg = ''
+            for char in line:
+                # comment
+                if char == '#':
+                    break
+                # correct pkg code on line
+                pkg += char
+            # remove excess whitespace
+            pkg = pkg.strip()
+            # pkg not empty
+            if pkg:
+                requirements.append(pkg)
+
+    # install given package
+    for package in requirements:
+        subprocess.call('pip install ' + package, shell=True)
