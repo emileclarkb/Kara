f7ca7a1a682840b99c45c2007b65aa7ae48dc26b
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-17 21:38:48 +1000

    simple color library

---
+++ src/Core/colors.py
@@ -0,0 +1,21 @@
+# ASSETS
+from sys import platform
+from termcolor import colored
+
+
+# on windows
+if platform == 'win32':
+    # install if not already
+    import colorama
+    # initialize
+    colorama.init()
+
+# print red text
+def red(value):
+    return colored(value, 'red', attrs=['bold'])
+# print green text
+def green(value):
+    return colored(value, 'green', attrs=['bold'])
+# print white text
+def white(value):
+    return colored(value, 'white', attrs=['bold'])
