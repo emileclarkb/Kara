a492072ae636412ed544697eeaa1117c4c741673
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-17 21:38:40 +1000

    ability inheritance

---
+++ src/Core/ability.py
@@ -0,0 +1,13 @@
+class Ability:
+    def __init__(self, name, target):
+        self.name = ""
+        self.target = ""
+        self.keywords = ""
+
+class Ability:
+    def __init_(self):
+        self.commands = []
+        self.exact = "" # set exact trigger
+
+    def command(self, data):
+        pass
