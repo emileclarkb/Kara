5a249106ab3b5262ea3c8463a4bcb05bd8e45dba
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-19 20:41:05 +1000

    linking for kara to abilities

---
+++ src/Core/Data/link.py
@@ -0,0 +1,4 @@
+from Core.Abilities.Kara-Time import currentDate
+from Core.Abilities.Kara-Time import currentMonth
+from Core.Abilities.Kara-Time import currentTime
+from Core.Abilities.Kara-Time import currentYear
