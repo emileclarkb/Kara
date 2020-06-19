c02726fb86575432dd68201ddd48ce32d1f84ca1
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-20 09:21:55 +1000

    added current time, current date today, tomorrow, or in X amount of days

--- src/Core/Abilities/Kara_Time/main.py
+++ src/Core/Abilities/Kara_Time/main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 # timezones
 #import pendulum
 
@@ -18,7 +18,7 @@ indicators = ['st', 'nd', 'rd', 'th']
 # time in the day
 def currentTime(Kara, command):
     # get current local time
-    now = datetime.now()
+    now = datetime.datetime.now()
 
     # remove all but hours and minutes
     hour = int(now.strftime("%H"))
@@ -38,10 +38,13 @@ def currentTime(Kara, command):
     if minute[0] == '0':
         # split for better speech
         # '05' -> '0 5'
-        minute = ' '.join(list(minute))
+        minute = list(minute)
+        minute[0] = 'O' # use o instead of 0 so voice says it correct
+        minute = ' '.join(minute)
+        # minute = ' '.join(list(minute))
 
     # format speech
-    Kara.speak('The Time is ' + str(hour) + minute + period)
+    Kara.speak('The Time is ' + str(hour) + ' ' + minute + period)
 
 
 # Today is Thursday the 18th
@@ -49,24 +52,46 @@ def currentTime(Kara, command):
 # date in the year
 def currentDate(Kara, command):
     # get date and split into year, month, and day
-    date = datetime.date(datetime.now())
-    day = date.day # select month and convert to int
+    date = datetime.datetime.date(datetime.datetime.now())
 
+    # customizable opening words
+    opening = 'Today is '
+
+    # move date to tomorrow
+    if 'tomorrow' in command:
+        date += datetime.timedelta(days=1)
+        # chaning opening for "tomorrow"
+        opening = 'Tomorrow it will be '
+    # date in an amount of days
+    # ie. "what's the date in 4 days?"
+    elif 'days' in command:
+        # split str to list
+        split = command.split()
+        # find where days was said
+        index = split.index('days')
+        days = split[index - 1] # get word before "days"
+        date += datetime.timedelta(days=int(days))
+
+        # chaning opening to be relevent
+        opening = 'In ' + days + ' days it will be '
+
+    day = date.day # select day
     # get relevent ordinal indicator
-    indicator = max(0, 3, day % 10 - 1) # clamp to stay within the list
+    # clamp to stay within the list
+    indicator = int(str(day)[-1]) - 1 # get final place and align with list (-1)
     indicator = indicators[indicator]
 
-    weekday = weekdays[date.weekday()]
+    weekday = weekdays[date.weekday()] # select weekday
 
     # format speech
-    Kara.speak('Today is ' + weekday + ' the ' +  day + indicator)
+    Kara.speak(opening + weekday + ' the ' +  str(day) + indicator)
 
 
 
 # month in the year
 def currentMonth(Kara, command):
     # get date and split into year, month, and day
-    date = datetime.date(datetime.now())
+    date = datetime.datetime.date(datetime.datetime.now())
     month = date.month # select month and convert to int
 
     # format speech
@@ -75,7 +100,7 @@ def currentMonth(Kara, command):
 # the current year
 def currentYear(Kara, command):
     # get date and split into year, month, and day
-    date = datetime.date(datetime.now())
+    date = datetime.datetime.date(datetime.datetime.now())
     year = str(date.year) # select year
 
     # split year for better speech
