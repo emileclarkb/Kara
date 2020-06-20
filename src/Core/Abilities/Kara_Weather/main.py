3e7fce11f0c6ef39c7cf244b1cc644a407db7346
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-20 17:10:56 +1000

    add basic weather command

--- src/Core/Abilities/Kara_Weather/main.py
+++ src/Core/Abilities/Kara_Weather/main.py
@@ -1,6 +1,16 @@
-# Kara.speak('Hey World!')
-# Kara.listen()
+# geolocation (ip to location)
+import geocoder
+# Kara Weather
+from Core.Abilities.Kara_Weather.weather import forcast
 
 
-def command(Kara, command):
-    pass
+def weather(Kara, command):
+    # get current location from ip
+    g = geocoder.ip('me')
+
+    # pass city
+    data = forcast(g.city, days=1)
+
+    line = 'Today in ' + g.city + 'you can expect ' + data['weather'][0]['weather']
+    # in brisbane you can expect light rain
+    Kara.speak(line)
