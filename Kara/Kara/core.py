4e7d61b60b2aa31c43851e6c3bd453d5f4095d6d
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-21 20:52:05 +1000

    move Ability and Integration creation to creation.py file

--- Kara/Kara/core.py
+++ Kara/Kara/core.py
@@ -1,15 +1,13 @@
 # Kara Object
 
 # native
-import os
 import json
 import importlib
-import pathlib
 # 3rd Party
 import pyttsx3
 import speech_recognition as sr
 # Kara
-from Kara.Scripts.colors import red, yellow, green
+from Kara.Kara.Scripts.colors import red
 
 
 class Kara:
@@ -44,7 +42,7 @@ class Kara:
         # handle if link was made incorrectly
         try:
             importlib.reload(self.link)
-        except NameError:
+        except AttributeError:
             print(red('\n[-] Cached Linking File Failed to Operate!'))
             sys.exit(1)
 
@@ -72,73 +70,6 @@ class Kara:
                 print("Exception: " + str(e))
 
         return text.lower()
-
-    # create new ability template
-    def abilityTemp(self, name):
-        # path to abilities
-        path = self.abilitiesPath + name
-
-        # make ability
-        try:
-            os.mkdir(path)
-        except FileExistsError:
-            # error message
-            return 1
-
-        # absolute path (so templating works with integration)
-        absPath = os.path.abspath(os.path.dirname(__file__))
-
-        # write main.py template
-        with open(absPath + 'Data/Templates/main.py', 'r') as template:
-            # write config
-            with open(path + '/main.py', 'w') as file:
-                # write template to main.py
-                file.write(template.read())
-
-        # write config.json file
-        # open template
-        with open(absPath + 'Data/Templates/config.json', 'r') as template:
-            # write config
-            with open(path + '/config.json', 'w') as file:
-                # write template to config.json
-                file.write(template.read())
-
-        # write requirements.txt file
-        # open template
-        with open(absPath + 'Data/Templates/requirements.txt', 'r') as template:
-            # write config
-            with open(path + '/requirements.txt', 'w') as file:
-                # write template to requirements.txt
-                file.write(template.read())
-
-        return 0
-
-
-    # init new integration
-    def integrate(self):
-        print(green('\n[+] Creating Default Paths...'))
-        # make default paths
-        try:
-            # abilities
-            os.mkdir(self.abilitiesPath)
-        except FileExistsError:
-            # error message
-            print(yellow('[!] Abilities Path Already Exists...'))
-        try:
-            # cache
-            os.mkdir(self.cachePath)
-        except FileExistsError:
-            # error message
-            print(yellow('[!] Cache Path Already Exists...'))
-
-        print(green('[+] Creating Cache Files...'))
-
-        # if file doens't exist
-        # write empty json file
-        with open(self.cachePath + 'abilities.json', 'w') as file:
-            file.write('{}') # empty json
-
-        print(green('\n[+] Successfully Instanced New Integration!'))
 
 
     def compile(self, text):
