47a707ba1e38585342d7b1f72315efa2f187c487
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-23 21:33:53 +1000

    add basic converation

---
+++ Kara/Core/Abilities/Kara_Conversational/main.py
@@ -0,0 +1,17 @@
+from random import choice
+
+greetings = ['hey', 'hi', 'hello', 'g\'day']
+status = ['amazing', 'i\'m great today! How are you?', 'spectacular', 'better than ever']
+thank = ['my pleasure', 'you\'re welcome', 'just doing my job', 'all good']
+
+# hello
+def greeting(Kara, command):
+    Kara.speak(choice(greetings))
+
+# how are you
+def howru(Kara, command):
+    Kara.speak(choice(status))
+
+# thanks
+def thanks(Kara, command):
+    Kara.speak(choice(thank))
