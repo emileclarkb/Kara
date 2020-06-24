4b7c215c044ac97a4d7275e7e460a519034e7cdd
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-24 18:40:32 +1000

    handle permission error

--- Kara/Core/Scripts/setup.py
+++ Kara/Core/Scripts/setup.py
@@ -1,23 +1,28 @@
 import subprocess
 import os
+from Core.Scripts.colors import red
 
 # download all modules given in a requirements file
 def setup(path):
-    # read requirements
-    with open(path, 'r') as file:
-        # iterate lines
-        for line in file.readlines():
-            pkg = ''
-            for char in line:
-                # comment support
-                if char == '#':
-                    break
-                # correct pkg code on line
-                pkg += char
-            # remove excess whitespace
-            pkg = pkg.strip()
-            # pkg not empty
-            if pkg:
-                # install package
-                subprocess.call('pip install ' + pkg, shell=True)
-                print() # empty trailing line
+    try:
+        # read requirements
+        with open(path, 'r') as file:
+            # iterate lines
+            for line in file.readlines():
+                pkg = ''
+                for char in line:
+                    # comment support
+                    if char == '#':
+                        break
+                    # correct pkg code on line
+                    pkg += char
+                # remove excess whitespace
+                pkg = pkg.strip()
+                # pkg not empty
+                if pkg:
+                    # install package
+                    subprocess.call('pip install ' + pkg, shell=True)
+                    print() # empty trailing line
+    # no access
+    except PermissionError:
+        print(red('\n[!] Permission Error Opening: \"' + path + '\"!'))
