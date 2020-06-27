d075402e491611bed0be9e2977a292431867c198
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-27 17:33:31 +1000

    added sin, cos, and tan

--- Kara/Core/Abilities/Kara_Math/main.py
+++ Kara/Core/Abilities/Kara_Math/main.py
@@ -2,7 +2,11 @@ import math
 
 # acceptable math terms
 accepted = '+-/**'
+# acceptable special strings
+special = ['math.pi', 'math.sin(', 'math.cos(', 'math.tan(']
 
+# root (special: square, cube)
+# sign cosine tangent
 
 # perform math operations
 # format command to become an equation
@@ -13,22 +17,40 @@ def maths(Kara, command):
     # replace strings with acceptable python
     command = command.replace(' x ', ' * ')
     command = command.replace(' pi ', ' math.pi ')
+    command = command.replace(' sign ', ' math.sin( ')
+    command = command.replace(' cosine ', ' math.cos( ')
+    command = command.replace(' tangent ', ' math.tan( ')
     command = command.replace(' ^ ', ' ** ')
     command = command.replace(' squared ', ' ** 2 ')
     command = command.replace(' cubed ', ' ** 3 ')
 
     eq = ''
 
+    # last amount of words that were functions
+    func = 0
+
     # remove unnecessary "words"
     for word in command.split():
         # is acceptable
-        if word == 'math.pi' or word in accepted or word.isdigit():
+        if word in special or word in accepted or word.isdigit():
             # add padding (white space)
             eq += word + ' '
 
+            # function discovered
+            if word in special:
+                func += 1
+            # not currently a function and functions previous
+            elif func:
+                # end functions
+                eq += ')' * func
+
+
+
+    print(eq)
     # evaluate string and round to four decimal places
     answer = eval(eq)
     answer = round(answer, 4)
+
 
     # format equation again for speech
     # *, /, math.pi,
@@ -39,5 +61,12 @@ def maths(Kara, command):
     speech = speech.replace(' ** 2 ', ' squared ')
     speech = speech.replace(' ** 3 ', ' cubed ')
     speech = speech.replace(' ** ', ' to the power of ')
+    speech = speech.replace('(', '')
+    speech = speech.replace(')', '')
+    speech = speech.replace(' math.sin ', ' sine of ')
+    speech = speech.replace(' math.cos ', ' cosine of ')
+    speech = speech.replace(' math.tan ', ' tangent of ')
+
+    print(speech)
 
     Kara.speak(speech + ' is ' + str(answer))
