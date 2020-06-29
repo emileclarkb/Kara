eff031dfa3a2c5293d709ae7f85d714833b972ca
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-29 12:14:12 +1000

    sample integration main file

---
+++ Kara/Core/Data/Templates/Integration/main.py
@@ -0,0 +1,18 @@
+# Kara
+from Kara import Kara
+from Kara.util import *
+from Kara.Core.compiler import *
+
+
+def main():
+    # paths default to 'Abilities/' and 'Cache/'
+
+    kara = Kara()
+    compile()
+
+    # kara.wake = ['Teresa']
+    # kara.manual = True
+
+
+if __name__ == '__main__':
+    main()
